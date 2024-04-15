50f338227ff1af7912c51c8e7321c39d5afccd32
Elisa <elisa.giubergia@itiscuneo.eu>
2024-04-15 12:01:19 +0200

    inizio implementazione

--- lib/visualizzaProdotti.dart
+++ lib/visualizzaProdotti.dart
@@ -1,20 +1,48 @@
+import 'package:cosmetici_2/prodotto.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'categoria.dart';
+import 'main.dart';
 
-class ShowProducts extends StatefulWidget{
-  ShowProducts({super.key, required this.categories});
-  List<CategoryA> categories;
+class ShowProducts extends StatelessWidget{
+  ShowProducts({super.key, required this.prodotti});
+  List<Prodotto> prodotti = [];
 
-  @override
-  State<StatefulWidget> createState(categories) {
-
-  }
-
-}
-
-class _ShowProductsState extends ShowProducts{
   Widget build(BuildContext context) {
-    return const Expanded(child: SingleChildScrollView());
+    return Scaffold(
+        appBar: AppBar(title: const Text("Cosmetici", style: TextStyle(color: Colors.white)), backgroundColor: Colors.purple,),
+        body: Column(children: [
+          Expanded(child: ListView.builder(
+            itemCount: prodotti.length,
 
-  }
-}
+            itemBuilder: (context, index){
+              return ListTile(
+                leading: Image.network(prodotti[index].imageLink!), // Immagine
+                title: Text(
+                  prodotti[index].name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('ID: ${prodotti[index].id}'),
+                    Text('Categoria: ${prodotti[index].category}'),
+                    Text(''),
+                  ],
+                ),
+                trailing: Icon(Icons.add_circle),
+                onTap: (){
+                  //Navigator.push(context, MaterialPageRoute(builder: (context) => xxx(libro: libri[index])));
+                },
+              );
+            },
+          ),
+
+          )
+
+
+        ]));
+
+  }}
