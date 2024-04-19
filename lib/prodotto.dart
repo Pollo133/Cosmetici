3805dbfe959e9b7249909c146ded2610ad1a3f49
Pollo133 <111647806+Pollo133@users.noreply.github.com>
2024-04-19 16:46:29 +0200

    Update prodotto.dart

--- lib/prodotto.dart
+++ lib/prodotto.dart
@@ -112,6 +112,15 @@ class Prodotto{
   set id(int value) {
     _id = value;
   }
+  
+  void checkImage() async {
+    final url = Uri.parse(imageLink);
+
+    http.Response response = await http.get(url);
+    if (response.statusCode != 200) {
+      _imageLink = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+    }
+  }
 
 }
 
